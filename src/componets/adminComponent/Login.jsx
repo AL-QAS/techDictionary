da8cf03cc307e416a5785c22b7f616408f3cf7e8
Mustapha Ibrahim <ibrahimmustapha1021gmail.com@gmail.com>
2024-06-07 07:53:37 +0100

    works in progress

--- src/componets/adminComponent/Login.jsx
+++ src/componets/adminComponent/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   <p className="ctn-1-p">Learn a new tech word everyday to become more fluent in the tech space lingual</p>
 </div>
     </div>
-    <div className="login-ctn-2">
+    <div className="login-ctn-22">
       
     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="custom-size-40" viewBox="0 0 16 16">
   <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>

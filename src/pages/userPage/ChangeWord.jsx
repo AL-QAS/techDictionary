c0d9768f9a62a4c7ffbc050964d21322eb1cb5e1
Qassim Rufai <rufaiqassim@gmail.com>
2024-06-12 11:59:02 +0100

    Revised progress

--- src/pages/userPage/ChangeWord.jsx
+++ src/pages/userPage/ChangeWord.jsx
@@ -21,18 +21,37 @@ setChangeWord(e.target.value)
   </div>
 
   <div className="feed__container" style={{padding:'20px'}}>
-    <h3>Request for change of word</h3>
+    <h3 style={{marginLeft:'60px',paddingBottom:'10px',fontWeight:'bold'}}>Request for change of word</h3>
   <div className="feed__container-content-box">
     <form onSubmit={()=>{}}>
      <div style={{display:'flex',flexDirection:'column',margin:'10px 0'}}>
-      <label>Word</label>
-      <input type='text' placeholder="input the word" onChange={handleChangeWord} value={changeWord} style={{paddingLeft:'10px',border:'1px solid #F5F5F5'}}/>
+      <label style={{color:'#8F8F8F',fontSize:'24px'}}>Word</label>
+      <input type='text' placeholder="input the word" onChange={handleChangeWord} value={changeWord} style={{padding:'10px',
+        border:'2px solid #8F8F8F',
+        fontSize:'20px',
+        borderRadius:'6px'
+        }}/>
      </div>
      <div style={{display:'flex',flexDirection:'column'}}>
-     <label>Description</label>
-      <input type='text' placeholder="input the description" onChange={handleChangeDescription} value={description} style={{minHeight:'100px',paddingLeft:'10px',border:'1px solid #F5F5F5'}}/>
+     <label style={{color:'#8F8F8F',fontSize:'24px'}}>Description</label>
+      <input type='text' placeholder="input the description" onChange={handleChangeDescription} value={description} style={{minHeight:'100px',
+        paddingLeft:'10px',
+        border:'2px solid #8F8F8F',
+        fontSize:'20px',
+        borderRadius:'6px'
+        }}/>
      </div>
-     <button type="submit" style={{width:'100%',textAlign:'center',backgroundColor:' #F5F5F5',marginTop:'10px'}}>Submit</button>
+     <button type="submit" style={{width:'100%',
+      textAlign:'center',
+      backgroundColor:' #F5F5F5',
+      marginTop:'10px',
+      border:'none',
+      padding:'4px',
+      textTransform:'uppercase',
+      fontSize:'24px',
+      lineHeight:'28.8px',
+      borderRadius:'6px'
+      }}>Submit</button>
     </form>
   </div>
 
